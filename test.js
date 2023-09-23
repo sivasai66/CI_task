a6591c5c0c75b11592a0e05cd21cd81b1b0aea4d
w7Pratham <prathamshingne@gmail.com>
2023-09-23 09:37:53 -0400

    Updated tests

--- test.js
+++ test.js
@@ -4,9 +4,19 @@ const assert = require('chai').assert;
 const app = require('./app'); // Import your app.js file
 
 describe('App', function () {
-    it('should return "Hello, Jenkins CI/CD!"', function (done) {
-        const server = http.createServer(app);
+    let server;
 
+    // Before running the tests, start the HTTP server
+    before(function () {
+        server = http.createServer(app).listen(3000); // Assuming your app runs on port 3000
+    });
+
+    // After all tests, close the server
+    after(function () {
+        server.close();
+    });
+
+    it('should return "Hello, Jenkins CI/CD!"', function (done) {
         // Send a request to the server
         http.get('http://localhost:3000', function (response) {
             let data = '';
@@ -24,5 +34,4 @@ describe('App', function () {
         });
     });
 });
-
 
